04ec4ab5f349f1afcf7220a09512c7ce483d587a
nesscollege2016 <nesscollege2016@gmail.com>
2016-06-02 09:32:50 +0300

    added songs

---
+++ app/src/main/java/ness/tomerbu/edu/recyclerviewdemos/Song.java
@@ -0,0 +1,16 @@
+package ness.tomerbu.edu.recyclerviewdemos;
+
+/**
+ * Created by android on 02/06/2016.
+ */
+public class Song {
+    String title;
+    int imageResId;
+    String artist;
+
+    public Song(String title, int imageResId, String artist) {
+        this.title = title;
+        this.imageResId = imageResId;
+        this.artist = artist;
+    }
+}
--- app/src/main/java/ness/tomerbu/edu/recyclerviewdemos/SongAdapter.java
+++ app/src/main/java/ness/tomerbu/edu/recyclerviewdemos/SongAdapter.java
@@ -6,16 +6,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by android on 02/06/2016.
  */
 public class SongAdapter extends RecyclerView.Adapter<SongViewHolder> {
 
+    private final ArrayList<Song> songs;
+
+    public ArrayList<Song> getSongs(){
+        ArrayList<Song> songs = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            songs.add(new Song("Hello", R.drawable.adele, "Adele"));
+            songs.add(new Song("Nothing like you", R.drawable.rihanna, "Rihanna"));
+            songs.add(new Song("Etmol Haya Tov", R.drawable.shlomo, "Shlomo Artzi"));
+            songs.add(new Song("Itay", R.drawable.keren, "Keren Palas"));
+        }
+        return songs;
+    }
     //constructor that get's the inflater
     LayoutInflater inflater;
     public SongAdapter(LayoutInflater inflater) {
         this.inflater = inflater;
+        this.songs = getSongs();
     }
 
     @Override
@@ -27,13 +43,16 @@ public class SongAdapter extends RecyclerView.Adapter<SongViewHolder> {
 
     @Override
     public void onBindViewHolder(SongViewHolder holder, int position) {
-        holder.tvSongName.setText("SongName " + position);
-        holder.tvArtist.setText("Artist " + position);
+        Song s = songs.get(position);
+        holder.tvSongName.setText(s.title);
+        holder.tvArtist.setText(s.artist);
+        holder.ivAlbum.setImageResource(s.imageResId);
+
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return songs.size();
     }
 }
 
@@ -51,6 +70,13 @@ class SongViewHolder extends RecyclerView.ViewHolder{
         ivAlbum = (ImageView) itemView.findViewById(R.id.ivAlbum);
         tvArtist = (TextView) itemView.findViewById(R.id.tvArtist);
         tvSongName = (TextView) itemView.findViewById(R.id.tvSongName);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = getAdapterPosition();
+                Toast.makeText(view.getContext(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
